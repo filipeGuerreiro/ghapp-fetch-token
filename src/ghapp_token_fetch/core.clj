6d8935884206ebfef09997343e8de880d8e421b1
filipe-guerreiro <filipe.guerreiro@linecorp.com>
2020-11-05 17:31:26 +0900

    Change JWT payload fields

--- src/ghapp_token_fetch/core.clj
+++ src/ghapp_token_fetch/core.clj
@@ -1,28 +1,38 @@
 (ns ghapp-token-fetch.core
   (:require [cli-matic.core :refer [run-cmd]]
-            [tentacles.core :refer [api-call]]
+            [tentacles.core :refer [api-call url]]
             [buddy.core.keys :refer [private-key]]
             [buddy.sign.jwt :refer [sign]]
-            [clj-time.core :as time])
+            [clj-time.core :as t]
+            [clj-time.coerce :as c])
   (:gen-class))
 
 (defn- gen-jwt [app-id pkey-file-path]
   (let [pkey    (private-key pkey-file-path)
-        payload {:iat time/now
-                 :exp (time/plus (time/now) (time/minutes 10))
+        parse-time #(long (/ (c/to-long %) 1000))
+        payload {:iat (parse-time (t/now))
+                 :exp (parse-time (t/plus (t/now) (t/minutes 10)))
                  :iss app-id}]
-    (sign payload pkey {:alg :es256})))
+    ;(println payload)
+    ;(println (sign payload pkey {:alg :rs256}))
+    (sign payload pkey {:alg :rs256})))
 
-(defn- fetch-token [{:keys [app-id installation-id pkey-file-path]}]
+(defn- fetch-token [{:keys [endpoint app-id installation-id pkey-file-path]}]
   (let [jwt     (gen-jwt app-id pkey-file-path)
         options {:oauth-token (str "Bearer " jwt)
                  :accept "application/vnd.github.v3+json"}]
-    (api-call :get "app/installations/%s/access_tokens" [installation-id] options)))
+    (binding [url endpoint]
+      (println
+       (api-call :get "app/installations/%s/access_tokens" [installation-id] options)))))
 
 (def CLI_CONFIG
   {:command     "ghapp-token-fetch"
    :runs        fetch-token
-   :opts        [{:as     "App ID"
+   :opts        [{:as "Github API endpoint"
+                  :option "endpoint" :short "e"
+                  :default "https://api.github.com/"
+                  :type :string}
+                 {:as     "App ID"
                   :option "app-id" :short "a"
                   :type   :int}
                  {:as      "Installation ID"
@@ -33,4 +43,4 @@
                   :type    :string}]})
 
 (defn -main [& args]
-  (println (run-cmd args CLI_CONFIG)))
+  (run-cmd args CLI_CONFIG))
